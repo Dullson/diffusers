060f2141f297ead0bdf2f05d417a156eed981032
Dullson <dullson@gmail.com>
2022-10-13 04:41:05 +0300

    feat(dreambooth): enable attention slicing

--- examples/dreambooth/train_dreambooth.py
+++ examples/dreambooth/train_dreambooth.py
@@ -347,6 +347,7 @@ def main():
 
             sample_dataloader = accelerator.prepare(sample_dataloader)
             pipeline.to(accelerator.device)
+            pipeline.enable_attention_slicing()
 
             for example in tqdm(
                 sample_dataloader, desc="Generating class images", disable=not accelerator.is_local_main_process
